2cd6a53d3b9c0397ce48cdc977436c846adaadd3
ebilui <ebina@re-view.co.jp>
2022-11-16 13:44:11 +0900

    update

--- create_tesagyou.py
+++ create_tesagyou.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import mojimoji
 
-df = pd.read_csv('./csv/バス停数.csv')
+df = pd.read_csv('./csv/バス停数 - バス停数.csv')
 df2 = pd.read_csv('./csv/kunai_data.csv')
 
 df2 = df2.replace('\(.+?\)', {'バス停名称':''}, regex=True)
@@ -11,6 +11,7 @@ counts = df2.pivot_table(index=['バス停名称'], aggfunc='size')
 # print(counts.loc['IHI前'])
 # print(counts.loc['2号バース前'])
 print(df2[df2['バス停名称']=='いかづち公園前'])
+ookii = 0
 # print(df2[df2['バス停名称']=='上野松坂屋前'].values.tolist()[0])
 for name in df['バス停名称']:
     gachi_name = df[df['バス停名称']==name]['バス停数'].to_list()[0]
@@ -23,6 +24,11 @@ for name in df['バス停名称']:
         value = df2[df2['バス停名称']==name].values.tolist()[0]
         # print(value)
         df2.loc[len(df2), df2.columns] = value
+    if int(gachi_name) < moto_name:
+        print('元ネームが大きいよ')
+        print(name)
+        ookii+=1
+print(ookii)
 # print(df2.pivot_table(index=['バス停名称'], aggfunc='size').loc['いかづち公園前'])
 # print(df2[df2['バス停名称']=='いかづち公園前'])
 df2.sort_values(by='バス停名称', inplace=True)
